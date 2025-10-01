8682b268ae7bdb33b1f92391ec0250ec79277752
MrWetsnow <509887+DanTulovsky@users.noreply.github.com>
2025-10-01 09:06:38 -0400

    fix script

--- scripts/compute-current-tag.sh
+++ scripts/compute-current-tag.sh
@@ -5,15 +5,6 @@ set -euo pipefail
 # Prints: NEW_TAG='vX.Y.Z'\nCOMMIT_HASH='...'\nBUILD_TIME='...'
 
 LATEST_TAG=$(git tag --sort=-v:refname | grep -E '^v[0-9]+\.[0-9]+\.[0-9]+$' | head -n1 || true)
-if [ -z "$LATEST_TAG" ]; then
-  NEW_TAG="v0.1.0"
-else
-  MAJOR=$(echo $LATEST_TAG | cut -d. -f1 | tr -d 'v')
-  MINOR=$(echo $LATEST_TAG | cut -d. -f2)
-  PATCH=$(echo $LATEST_TAG | cut -d. -f3)
-  PATCH=$((PATCH + 1))
-  NEW_TAG="v${MAJOR}.${MINOR}.${PATCH}"
-fi
 
 # Only print the next tag to stdout (caller may compute other metadata)
-printf "%s" "$NEW_TAG"
+printf "%s" "$LATEST_TAG"
